85234e2f0ecfee25dc5ffcb45867ed77e10b13ce
khanshorif331 <khanshorif331@gmail.com>
2022-10-04 00:23:57 +0600

    navbar doone

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-function App() {
+import { Container } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import { Route, Routes } from 'react-router-dom'
+import { About } from './pages/About'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { Navbar } from './components/Navbar'
 
-  return (
-    <h1>Hey what's up</h1>
-  )
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Container className="mb-4">
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/store" element={<Store />}></Route>
+					<Route path="/about" element={<About />}></Route>
+				</Routes>
+			</Container>
+		</>
+	)
 }
 
 export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Container, Nav, Navbar as NavbarBs, NavLink } from 'react-bootstrap'
+export const Navbar = () => {
+	return (
+		<NavbarBs className="bg-white shadow-sm mb-3">
+			<Container>
+				<Nav>
+					<Nav.Link to="/" as={NavLink}>
+						Home
+					</Nav.Link>
+					<Nav.Link to="/store" as={NavLink}>
+						Store
+					</Nav.Link>
+					<Nav.Link to="/about" as={NavLink}>
+						About
+					</Nav.Link>
+				</Nav>
+			</Container>
+		</NavbarBs>
+	)
+}
