aeaa0e25ece92158507aede11a9192ab4721c2ce
khanshorif331 <khanshorif331@gmail.com>
2022-10-04 00:42:30 +0600

    bug fixed with the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import { Container, Nav, Navbar as NavbarBs, NavLink } from 'react-bootstrap'
+import { Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
 export const Navbar = () => {
 	return (
 		<NavbarBs className="bg-white shadow-sm mb-3">
 			<Container>
-				<Nav>
+				<Nav className="me-auto">
 					<Nav.Link to="/" as={NavLink}>
 						Home
 					</Nav.Link>
@@ -14,6 +16,7 @@ export const Navbar = () => {
 						About
 					</Nav.Link>
 				</Nav>
+				hi
 			</Container>
 		</NavbarBs>
 	)
