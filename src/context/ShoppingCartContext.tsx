2c1d4ee01cdb0fe80d48ec2c408316e15f0cdd27
khanshorif331 <khanshorif331@gmail.com>
2022-10-05 23:41:17 +0600

    created context api in the shopping cart

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,21 @@
+import { createContext, ReactNode, useContext } from 'react'
+
+type ShoppingCartProviderProps = {
+	children: ReactNode
+}
+
+const ShoppingCartContext = createContext({})
+
+export const useShoppingCart = () => {
+	return useContext(ShoppingCartContext)
+}
+
+export const ShoppingCartProvider = ({
+	children,
+}: ShoppingCartProviderProps) => {
+	return (
+		<ShoppingCartContext.Provider value={{}}>
+			{children}
+		</ShoppingCartContext.Provider>
+	)
+}
