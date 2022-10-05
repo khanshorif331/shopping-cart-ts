f10a02858d6c41fd5ac93c75bfc8b8ad0d61463b
khanshorif331 <khanshorif331@gmail.com>
2022-10-05 20:49:55 +0600

    made json data of items and created new component fr mapping

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,10 @@
+type StoreItemProps = {
+	id: number
+	name: string
+	price: number
+	imgurl: string
+}
+
+export const StoreItem = ({ id, name, price, imgurl }: StoreItemProps) => {
+	return <div> hell0</div>
+}
