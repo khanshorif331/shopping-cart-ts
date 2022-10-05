a702a85a71edc1d3782e5fcf0a91ff8ede86cb04
khanshorif331 <khanshorif331@gmail.com>
2022-10-06 01:05:17 +0600

    getItemsquantity and increate cart quantity functions have been created

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import { About } from './pages/About'
 import { Home } from './pages/Home'
 import { Store } from './pages/Store'
 import { Navbar } from './components/Navbar'
+import { ShoppingCartProvider } from './context/ShoppingCartContext'
 
 function App() {
 	return (
 		<>
-			<Navbar />
-			<Container className="mb-4">
-				<Routes>
-					<Route path="/" element={<Home />}></Route>
-					<Route path="/store" element={<Store />}></Route>
-					<Route path="/about" element={<About />}></Route>
-				</Routes>
-			</Container>
+			<ShoppingCartProvider>
+				<Navbar />
+				<Container className="mb-4">
+					<Routes>
+						<Route path="/" element={<Home />}></Route>
+						<Route path="/store" element={<Store />}></Route>
+						<Route path="/about" element={<About />}></Route>
+					</Routes>
+				</Container>
+			</ShoppingCartProvider>
 		</>
 	)
 }
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,10 +1,22 @@
-import { createContext, ReactNode, useContext } from 'react'
+import { createContext, ReactNode, useContext, useState } from 'react'
 
 type ShoppingCartProviderProps = {
 	children: ReactNode
 }
 
-const ShoppingCartContext = createContext({})
+type CartItem = {
+	id: number
+	quantity: number
+}
+
+type ShoppingCartContext = {
+	getItemQuantity: (id: number) => number
+	increaseCartQuantity: (id: number) => void
+	decreaseCartQuantity: (id: number) => void
+	removeFromCart: (id: number) => void
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
 export const useShoppingCart = () => {
 	return useContext(ShoppingCartContext)
@@ -13,8 +25,32 @@ export const useShoppingCart = () => {
 export const ShoppingCartProvider = ({
 	children,
 }: ShoppingCartProviderProps) => {
+	const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+	const getItemQuantity = (id: number) => {
+		return cartItems.find(item => item.id === id)?.quantity || 0
+	}
+
+	const increaseCartQuantity = (id: number) => {
+		setCartItems(currItems => {
+			if (currItems.find(item => item.id === id) == null) {
+				return [...currItems, { id, quantity: 1 }]
+			} else {
+				return currItems.map(item => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity + 1 }
+					} else {
+						return item
+					}
+				})
+			}
+		})
+	}
+
 	return (
-		<ShoppingCartContext.Provider value={{}}>
+		<ShoppingCartContext.Provider
+			value={{ getItemQuantity, increaseCartQuantity }}
+		>
 			{children}
 		</ShoppingCartContext.Provider>
 	)
