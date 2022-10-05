fb18febfb32f823ed6b9dbff271f3d8885944d12
khanshorif331 <khanshorif331@gmail.com>
2022-10-05 21:13:16 +0600

    currency formatter used in the price sectin

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,6 @@
+import { Card } from 'react-bootstrap'
+import { formatCurrency } from '../utilities/formatCurrency'
+
 type StoreItemProps = {
 	id: number
 	name: string
@@ -6,5 +9,20 @@ type StoreItemProps = {
 }
 
 export const StoreItem = ({ id, name, price, imgurl }: StoreItemProps) => {
-	return <div> hell0</div>
+	return (
+		<Card>
+			<Card.Img
+				variant="top"
+				src={imgurl}
+				height="200px"
+				style={{ objectFit: 'contain' }}
+			/>
+			<Card.Body className="d-flex flex-column">
+				<Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+					<span className="fs-2">{name}</span>
+					<span className="ms-2 text-muted">{formatCurrency(price)}</span>
+				</Card.Title>
+			</Card.Body>
+		</Card>
+	)
 }
