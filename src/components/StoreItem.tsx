eec93387afddfce96c120c1f1b10e9f652a76924
khanshorif331 <khanshorif331@gmail.com>
2022-10-05 21:29:06 +0600

    initial design done for the stoore page

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 import { formatCurrency } from '../utilities/formatCurrency'
 
 type StoreItemProps = {
@@ -9,8 +9,9 @@ type StoreItemProps = {
 }
 
 export const StoreItem = ({ id, name, price, imgurl }: StoreItemProps) => {
+	const quantity = 0
 	return (
-		<Card>
+		<Card className="h-100">
 			<Card.Img
 				variant="top"
 				src={imgurl}
@@ -22,6 +23,30 @@ export const StoreItem = ({ id, name, price, imgurl }: StoreItemProps) => {
 					<span className="fs-2">{name}</span>
 					<span className="ms-2 text-muted">{formatCurrency(price)}</span>
 				</Card.Title>
+				<div className="mt-auto">
+					{quantity === 0 ? (
+						<Button className="w-100">+ Add To Cart</Button>
+					) : (
+						<div
+							className="d-flex align-items-center flex-column"
+							style={{ gap: '.5rem' }}
+						>
+							<div
+								className="d-flex justify-content-center align-content-center"
+								style={{ gap: '.5rem' }}
+							>
+								<Button>-</Button>
+								<div>
+									<span className="fs-3">{quantity}</span> in cart
+								</div>
+								<Button>+</Button>
+							</div>
+							<Button variant="danger" size="sm">
+								Remove
+							</Button>
+						</div>
+					)}
+				</div>
 			</Card.Body>
 		</Card>
 	)
