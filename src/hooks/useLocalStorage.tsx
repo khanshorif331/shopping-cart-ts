16b136a5625af46c0b467b8081eb5fd0f36164ab
khanshorif331 <khanshorif331@gmail.com>
2022-10-09 18:47:03 +0600

    useLocal stoorage bug fixed

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+// T indicates generic type means any type actually
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+	const [value, setValue] = useState<T>(() => {
+		const jsonValue = localStorage.getItem(key)
+		if (jsonValue != null) return JSON.parse(jsonValue)
+
+		if (typeof initialValue === 'function') {
+			return (initialValue as () => T)()
+		} else {
+			return initialValue
+		}
+	})
+}
